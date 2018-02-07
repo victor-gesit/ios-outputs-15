f4f11f09b745ececd97c38406d7f6da31280efd5
Victor Idongesit <victoridongesit@gmail.com>
2018-02-07 11:06:08 +0100

    solution to output 15

--- Output15/ViewController.swift
+++ Output15/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gestureSheet: UIImageView!
+    @IBOutlet weak var gestureTypeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        gestureSheet.isUserInteractionEnabled = true
+        let tapGestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(gestureDetected))
+        tapGestureRecogniser.name = "tap"
+        let doubleTapGestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(gestureDetected))
+        doubleTapGestureRecogniser.numberOfTapsRequired = 2
+        doubleTapGestureRecogniser.name = "doubleTap"
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(gestureDetected))
+        pinchGestureRecognizer.name = "pinch"
+        let rotationGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(gestureDetected))
+        rotationGestureRecognizer.name = "rotation"
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(gestureDetected))
+        longPressGestureRecognizer.name = "longPress"
+        gestureSheet.addGestureRecognizer(tapGestureRecogniser)
+        gestureSheet.addGestureRecognizer(doubleTapGestureRecogniser)
+        gestureSheet.addGestureRecognizer(pinchGestureRecognizer)
+        gestureSheet.addGestureRecognizer(rotationGestureRecognizer)
+        gestureSheet.addGestureRecognizer(longPressGestureRecognizer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func gestureDetected(recognizer: UIGestureRecognizer) {
+        if let gestureName = recognizer.name {
+            switch gestureName {
+                case "tap": gestureTypeLabel.text = "You just tapped"
+                case "doubleTap": gestureTypeLabel.text = "You double tapped"
+                case "swipeRight": gestureTypeLabel.text = "You just swiped right"
+                case "swipeLeft": gestureTypeLabel.text = "You just swiped left"
+                case "swipeUp": gestureTypeLabel.text = "You just swiped up"
+                case "swipeDown": gestureTypeLabel.text = "You just swiped down"
+                case "pinch": gestureTypeLabel.text = "You just pinched"
+                case "rotation": gestureTypeLabel.text = "You just rotated"
+                case "longPress": gestureTypeLabel.text = "You just long pressed"
+                case "pan": gestureTypeLabel.text = "You just panned"
+                default: gestureTypeLabel.text = "Unrecognized Gesture"
+            }
+        } else {
+            gestureTypeLabel.text = "Unrecognized Gesture"
+        }
     }
-
-
 }
 
